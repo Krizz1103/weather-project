dee971535b53794c8a43a2085f3f5ff115ed766e
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2023-06-27 17:10:59 +0100

    moved existing project

---
+++ src/index.js
@@ -0,0 +1,65 @@
+let now = new Date();
+let currentTime = document.querySelector("#currentTime");
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+
+currentTime.innerHTML = `${day} ${date} ${month} ${year} , ${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  let currentCity = document.querySelector("#currentCity");
+
+  currentCity.innerHTML = `${searchInput.value}`;
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let city = document.querySelector("#search-text-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function showTemperature(response) {
+  console.log(response);
+  let cityHeading = document.querySelector("#search-text-input");
+  cityHeading.innerHTML = response.data.city;
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector("#currentTemperature");
+  currentTemperature.innerHTML = `currently ${temperature}ºC`;
+  let wind = Math.round(response.data.wind.speed);
+  currentWind.innerHTML = `Windspeed ${wind} km/h`;
+}
